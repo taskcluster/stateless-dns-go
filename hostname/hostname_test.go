96d997bbb5279345ab429fd78eb7bb6c3ac9301a
Pete Moore <pmoore@mozilla.com>
2015-11-27 21:13:47 +0100

    Initial implementation

---
+++ hostname/hostname_test.go
@@ -0,0 +1,16 @@
+package hostname_test
+
+import (
+	"net"
+	"time"
+
+	"github.com/taskcluster/stateless-dns-go/hostname"
+)
+
+func ExampleNew() {
+	ip := net.IPv4(byte(203), byte(43), byte(55), byte(2))
+	subdomain := "foo.com"
+	expires := time.Now().Add(15 * time.Minute)
+	secret := "turnip4tea"
+	hostname.New(ip, subdomain, expires, secret)
+}
